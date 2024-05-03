a974225132c3befd9014bf3ff30b84a34ae3e100
Cubolink <70733191+Cubolink@users.noreply.github.com>
2024-05-02 23:51:16 -0400

    misc: Updated setup
    These are the steps I did to get this new environment without mm3d_pn2.
    These steps include more dependencies because I'm trying to have the
    dependencies that I used for my custom experiment of two
    convolutional occupancy networks.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,64 @@
-conda create -n pointattn python=3.8
-conda activate pointattn
-python -m pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
-python -m pip install h5py==3.6.0
-python -m pip install matplotlib==3.4.3
-python -m pip install munch==2.5.0
-python -m pip install open3d==0.13.0
-python -m pip install pyyaml
+# installing with torch
+conda create -n pointattn2 python=3.8 pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
+# installing torch3d
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath
+conda install pytorch3d -c pytorch3d
+
+
+# installing pytorch-scatter. Please make sure torch is not downgraded to CPU.
+conda install pytorch-scatter -c pyg  # TRY THIS FIRST. It shouldn't downgrade torch, for me it didnt at least (But it did when I was trying pytorch 2.x)
+# conda install pytorch_scatter -c conda-forge  # USE THIS ONE ONLY IF THE OTHER TRIED TO DOWNGRADE TORCH.
+
+
+# installing other dependencies (1)
+conda install trimesh -c conda-forge
+conda install tensorboardx
+conda install pandas scikit-image scipy
+conda install pyembree -c conda-forge
+conda install cython imageio numpy numpy-base matplotlib matplotlib-base pillow pytest pyyaml tqdm
+
+
+# installing other dependencies (2)
+conda install h5py
+conda install plyfile -c conda-forge
+conda install pyyaml
+
+
+# installing other dependencies (3)
+conda install tensorpack -c hcc
+
+
+# installing other dependencies (4)
+conda install transforms3d -c conda-forge
+conda install munch -c conda-forge
+conda install scikit-learn
+
+
+# installing open3d
+python -m pip install open3d==0.18.0  # an outdated version in conda caused me a problem with sklearn, that derived in failing installing the mmdet3d
+
+# stuff originally compiled from convolutional_occupancy_networks, but here installed with pip/conda
+conda install pykdtree -c conda-forge  # may take some time
+conda install pymcubes -c conda-forge
+# libmesh seems to be uneeded, because it's used in evaluation
+# libmise is used in generation, so it's NEEDED. I will install it later tho.
+# libsimplify is not used in my experiments, but it is in generation
+# libvoxelide is irrelevant, it's in generation but I don't handle voxels
+
+
+# installing tensorflow
+python -m pip install tensorflow  # this downgraded h5py to 3.11
+
+
+# installing mim's related stuff
+python -m pip install openmim
+python -m mim install mmcv mmdet
+python -m mim install mmsegmentation
+
+python -m pip install mmdet3d  # works now, with open3d 0.18
+
 
 # then, compile the utils as the README.md says
 cd utils/ChamferDistancePytorch/chamfer3D || exit
 python setup.py install
 cd ../../..
-
-cd utils/mm3d_pn2  || exit
-python setup.py build_ext --inplace
-cd ../..
-
-# The beginning of the end, we have to install things that for some reason are not in the requirements.txt
-python -m pip install transforms3d
-python -m pip install tensorpack
-python -m pip install tensorflow
-
-# At last, the troubling part
-python -m pip install openmim
-python -m pip install mmcv==0.6.2 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9/index.html
-python -m pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9/index.html
-
